874d22c2d3bf5bfa9b2cc51b4e5399a45a2e9fb6
ch-enfuse <chr@enfuse.io>
2023-01-30 11:05:23 -0800

    Add config to nginx to talk to backend

--- src/client/storeGeniusClient.ts
+++ src/client/storeGeniusClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 export const storeClient = axios.create({
-    baseURL: 'http://localhost:8081/store-genius',
+    baseURL: '/store-genius',
     timeout: 1000,
   });
 

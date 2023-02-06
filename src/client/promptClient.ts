cc36d366995cc06602d41f64b0f9230aa7a0440e
ch-enfuse <chr@enfuse.io>
2023-02-06 08:26:33 -0800

    Change base URL for beer demo client

--- src/client/promptClient.ts
+++ src/client/promptClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 export const promptClient = axios.create({
-    baseURL: 'http://localhost:8080/prompt',
+    baseURL: '/prompt',
     timeout: 10000,
   });
 

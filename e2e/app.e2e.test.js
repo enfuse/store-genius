6a81437e1c74e9e690135bcfec8bf4c6e692afcd
Tom Butler <tb@enfuse.io>
2023-02-23 18:21:02 -0700

    Add feedback form test

--- e2e/app.e2e.test.js
+++ e2e/app.e2e.test.js
@@ -8,3 +8,19 @@ test('Homepage should have correct title', async ({ page }) => {
     await navigateToHomePage(page)
     await expect(page).toHaveTitle(/Store Genius/)
 })
+
+test('Feedback form should not submit if invalid email', async ({ page }) => {
+
+    await navigateToHomePage(page)
+    await page.fill('input[name=email]', 'some@account');
+    await page.fill('textarea[name=message]', 'this is a test message');
+
+    //const button = await page.$('#\\:r3\\:');
+    // Click the button
+    //await button.click();
+
+    await page.click('text=Send!')
+    await expect(page.$('#\\:r1\\:-helper-text')).toHaveText('Email is invalid');
+
+    await page.waitForTimeout(1000);
+})
